0d531fcd2a3490f30dd68027aeb3c3639c42e77f
Wiktor Flis <wikflis@gmail.com>
2022-02-26 15:47:59 +0100

    Finish classes of Light

--- opengl-renderer/src/Light.h
+++ opengl-renderer/src/Light.h
@@ -6,10 +6,13 @@
 
 class Light {
     public:
+    Light();
     virtual void use(Shader& shader) = 0;
-    void setLightColors(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular);
+    void setLightColors(const glm::vec3 ambient, const glm::vec3 diffuse, const glm::vec3 specular);
     void setLightAttenuation(float constant, float linear, float quadratic);
-    void setUniformName(std::string name);
+    void setUniformName(const std::string& name);
+    void useLightProperties(Shader& shader);
+    std::string getPropertyName(const std::string& propertyName) const;
 
     std::string uniformName;
     glm::vec3 ambient;
@@ -24,15 +27,17 @@ class Light {
 class PositionalLight : public Light {
     public:
     glm::vec3 position;
+    const std::string uniformName;
 
-    PositionalLight(glm::vec3 position);
+    PositionalLight(std::string name, const glm::vec3 position);
     void use(Shader& shader);
 };
 
 class DirectionalLight : public Light {
     public:
     glm::vec3 direction;
+    const std::string uniformName;
 
-    DirectionalLight(glm::vec3 direction);
+    DirectionalLight(std::string name, const glm::vec3 direction);
     void use(Shader& shader);
 };
